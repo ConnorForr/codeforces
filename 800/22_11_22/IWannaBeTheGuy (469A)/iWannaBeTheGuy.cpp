e2021378085b3bfdd8d47d81face0218bea63f56
Connor <connor.forristal@gmail.com>
2022-11-22 14:20:27 -0600

    Ones from during class.

---
+++ 800/22_11_22/IWannaBeTheGuy (469A)/iWannaBeTheGuy.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,p;
+    cin>>n>>p;
+
+    p*=2;
+
+    set<int> set1;
+
+    while (p--) {
+        int a;
+        cin>>a;
+        set1.insert(a);
+    }
+
+    for (int i = 1; i<=n; i++) {
+        if(!(set1.find(i) != set1.end())) {
+            cout<<"Oh, my keyboard!";
+            return 0;
+        }
+    }
+    cout<<"I become the guy.";
+    return 0;
+}
---
+++ 800/22_11_22/IsYourHorseshoeOnTheOtherHoof (228A)/isYourHorseshoeOnTheOtherHoof.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+
+    int arr[4] = {a, b, c, d};
+    int count = 0;
+    for(int i = 0; i < 3; i++) {
+        if (arr[i] == arr[i + 1]) {
+            count++;
+        }
+    }
+    cout<<count;
+    return 0;
+}
